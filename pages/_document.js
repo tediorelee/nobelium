5ec3701133429f33a912f2af9033ee8b9ab55335
TEDIORELEE <tediorelee@qq.com>
2021-05-17 22:26:47 +0800

    update favicon

--- pages/_document.js
+++ pages/_document.js
@@ -83,7 +83,7 @@ class MyDocument extends Document {
             </>
           )}
 
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/avatar.png" />
           <link rel="icon" href="/favicon.svg" type="image/svg+xml"></link>
           {BLOG.analytics && BLOG.analytics.provider === 'ackee' && (
             <script
